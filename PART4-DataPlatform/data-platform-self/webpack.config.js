771295437373a59b22832d95931488a9795a666f
rayallen20 <452857163@qq.com>
2025-03-15 23:35:07 +0800

    项目工程化完成

---
+++ PART4-DataPlatform/data-platform-self/webpack.config.js
@@ -0,0 +1,83 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: {
+        index: './src/index.js',
+        common: './src/common.js',
+        login: './src/login.js',
+        register: './src/register.js',
+        student: './src/student.js',
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js',
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './public/index.html'),
+            filename: 'index.html',
+            chunks: [
+                'common',
+                'index',
+            ],
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './public/login.html'),
+            filename: 'login.html',
+            chunks: [
+                'common',
+                'login',
+            ],
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './public/register.html'),
+            filename: 'register.html',
+            chunks: [
+                'common',
+                'login',
+            ],
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './public/student.html'),
+            filename: 'student.html',
+            chunks: [
+                'common',
+                'student',
+            ],
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset',
+                generator: {
+                    filename: 'asset/images/[name].[hash][ext]',
+                }
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i, // 处理字体文件
+                type: 'asset/resource', // 使用资源模块
+                generator: {
+                    filename: 'asset/fonts/[name].[hash][ext]', // 输出路径和文件名
+                },
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader'],
+            }
+        ]
+    },
+    devtool: 'inline-source-map',
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        }
+    }
+}
